193de35deb7014628cfc4af6d6b007a8bbd9549f
Jianfeng Li <modifyljf@gmail.com>
2018-07-23 15:27:47 -0400

    Controller Processing.

--- src/Console/Commands/JsMakeCommand.php
+++ src/Console/Commands/JsMakeCommand.php
@@ -232,7 +232,7 @@ class JsMakeCommand extends GeneratorCommand
         return str_replace(
             ['DummyModel'],
             [$modelName],
-            file_get_contents(__DIR__ . "/stubs/make/resources/${template}/assets/js/webpack.mix.js")
+            file_get_contents(__DIR__ . "/stubs/make/resources/${template}/assets/webpack.mix.js")
         );
     }
 }
