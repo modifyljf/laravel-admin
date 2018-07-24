408212463e13d84daf22ca3db72a4026ccdfcfb9
Jianfeng Li <modifyljf@gmail.com>
2018-07-24 17:11:32 -0400

    Controller Processing.

--- src/Console/Commands/stubs/make/resources/metronic/assets/js/helpers/axios.js
+++ src/Console/Commands/stubs/make/resources/metronic/assets/js/helpers/axios.js
@@ -1,6 +1,23 @@
 import * as App from "../config/app";
 import toastr from "toastr";
 import _ from "lodash";
+import axios from 'axios';
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 // Init toastr options.
 toastr.options = {
@@ -22,7 +39,7 @@ toastr.options = {
 };
 
 // Add a response interceptor
-window.axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use(function (response) {
     // Do something with response data
     return response;
 }, function (error) {
@@ -52,3 +69,5 @@ window.axios.interceptors.response.use(function (response) {
     // Do something with response error
     return Promise.reject(error);
 });
+
+export default axios;
