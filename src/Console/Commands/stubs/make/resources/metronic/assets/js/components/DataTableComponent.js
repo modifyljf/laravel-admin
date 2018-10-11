84b11e39905e76eb916a4b1da3e9fefb1308d0c1
JianfengLi <modifyljf@gmail.com>
2018-10-11 16:23:01 -0400

    Set up the default table height.

--- src/Console/Commands/stubs/make/resources/metronic/assets/js/components/DataTableComponent.js
+++ src/Console/Commands/stubs/make/resources/metronic/assets/js/components/DataTableComponent.js
@@ -271,7 +271,7 @@ class DataTableComponent extends React.PureComponent {
                     webstorage: false,
                 }
             },
-            layout: {scroll: true, footer: true, height: 200},
+            layout: {scroll: true, footer: true},
             sortable: true,
             pagination: true,
             toolbar: {items: {pagination: {pageSizeSelect: [10, 20, 30, 50, 100]}}},
